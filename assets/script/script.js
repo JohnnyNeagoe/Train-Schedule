45a5fa69c6fcb1a0cb6ee243a480e7638aab1de7
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-07-05 22:44:16 -0400

    03

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,21 +12,45 @@ var database = firebase.database();
 
 $("#buttonSubmit").on("click", function(event) {
     event.preventDefault();
-
-    var empName = $("#name-input").val().trim();
-    var empDest = $("#destination-input").val().trim();
-    var empTime = $("#time-input").val().trim();
-    var empFreq = $("#frequency-input").val().trim();
-
-    var newTrain = {
-        train: empName,
-        destination: empDest,
-        time: empTime,
-        frequency: empFreq
-    }
+        var empName = $("#name-input").val().trim();
+        var empDest = $("#destination-input").val().trim();
+        var empTime = $("#time-input").val().trim();
+        var empFreq = $("#frequency-input").val().trim();
+        var newTrain = {
+            train: empName,
+            destination: empDest,
+            time: empTime,
+            frequency: empFreq
+        }
     database.ref().push(newTrain);
-    console.log(newTrain.train);
-    console.log(newTrain.destination);
-    console.log(newTrain.time);
-    console.log(newTrain.frequency);
+console.log(newTrain.train);
+console.log(newTrain.destination);
+console.log(newTrain.time);
+console.log(newTrain.frequency);
+    $("#name-input").val("");
+    $("#destination-input").val(""); 
+    $("#time-input").val(""); 
+    $("#frequency-input").val(""); 
+});
+
+database.ref().on("child_added", function(nextTrain){
+console.log(nextTrain.val());
+    nextTrain.preventDefault();
+        var empName = nextTrain.val().train;
+        var empDest = nextTrain.val().destination;
+        var empTime = nextTrain.val().time;
+        var empFreq = nextTrain.val().frequency;
+console.log(empName);
+console.log(empDest);
+console.log(empTime);
+console.log(empFreq);
+
+        var newRow = $("<tr>").append(
+            $("<td>").text(empName),
+            $("<td>").text(empDest),
+            $("<td>").text(empFreq),
+            $("<td>").text(empNextArrival),
+            $("<td>").text(empMinAway),
+        );
+        $(".table > tbody").append(newRow);
 });
